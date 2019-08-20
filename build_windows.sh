eac17c141f9b80ed530355cc52c800e763cc68e5
ta264 <ta264@users.noreply.github.com>
2019-08-20 06:30:59 +0100

    Hopefully fix System.Data.SQLite, another go at windows dll

--- build_windows.sh
+++ build_windows.sh
@@ -5,6 +5,5 @@ set -e
 source get_source.sh
 cd src
 
-SQLITE_OPTIONS_WIN=${SQLITE_OPTIONS//-/\/}
-cl ${SQLITE_OPTIONS} sqlite3.c -link -dll -out:sqlite3.dll
+cl sqlite3.c -link -dll -out:sqlite3.dll
 file sqlite3.dll
