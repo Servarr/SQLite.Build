6ca4e07e4c6b4c3da08313961e6a397fc8e15977
ta264 <ta264@users.noreply.github.com>
2019-08-22 22:24:59 +0100

    Create a net46 nuget.  Add in multiple windows runtimes

--- build_macos.sh
+++ build_macos.sh
@@ -4,4 +4,4 @@ set -e
 
 source get_sqlite.sh
 cd src
-gcc -v -O2 -dynamiclib -o libsqlite3.0.dylib ${SQLITE_OPTIONS} sqlite3.c
+gcc -v -O2 -dynamiclib -o libsqlite3.dylib ${SQLITE_OPTIONS} sqlite3.c
--- build_windows.sh
+++ build_windows.sh
@@ -4,5 +4,9 @@ set -e
 
 YEAR=2019
 VERSION=3280000
-curl --output sqlite.zip http://www.sqlite.org/${YEAR}/sqlite-dll-win32-x86-${VERSION}.zip
-unzip sqlite.zip -d src
+
+curl --output sqlite-x86.zip http://www.sqlite.org/${YEAR}/sqlite-dll-win32-x86-${VERSION}.zip
+unzip sqlite-x86.zip -d src/win-x86
+
+curl --output sqlite-x64.zip http://www.sqlite.org/${YEAR}/sqlite-dll-win64-x64-${VERSION}.zip
+unzip sqlite-x64.zip -d src/win-x64
