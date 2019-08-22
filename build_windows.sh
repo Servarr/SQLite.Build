10fdfae8ca46c09466ba7600f8ff17786bdc3df2
ta264 <ta264@users.noreply.github.com>
2019-08-22 22:27:40 +0100

    Fix windows

--- build_windows.sh
+++ build_windows.sh
@@ -5,6 +5,8 @@ set -e
 YEAR=2019
 VERSION=3280000
 
+mkdir src
+
 curl --output sqlite-x86.zip http://www.sqlite.org/${YEAR}/sqlite-dll-win32-x86-${VERSION}.zip
 unzip sqlite-x86.zip -d src/win-x86
 
