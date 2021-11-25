bd88a4af25597c219abdb89be3ede7ad58e7d031
ta264 <ta264@users.noreply.github.com>
2021-11-25 21:47:24 +0000

    Work around macOS arm64 varargs issues
    See https://github.com/dotnet/runtime/issues/48752
    https://github.com/dotnet/runtime/issues/48796

--- get_system_data_sqlite.sh
+++ get_system_data_sqlite.sh
@@ -8,3 +8,8 @@ cd net
 echo "$URL"
 curl --output system.data.sqlite.zip "https://system.data.sqlite.org/blobs/$1/sqlite-netFx-source-$1.zip"
 unzip system.data.sqlite.zip
+
+dos2unix System.Data.SQLite/SQLite3.cs
+dos2unix System.Data.SQLite/UnsafeNativeMethods.cs
+cd ..
+patch -p0 < patches/System.Data.SQLite/0001_osx-arm64_support.patch
