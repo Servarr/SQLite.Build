cac2265dd5461d662b770c5e4e630fbb6a0451b8
ta264 <ta264@users.noreply.github.com>
2021-11-27 13:37:59 +0000

    Better patch from Taloth

--- get_system_data_sqlite.sh
+++ get_system_data_sqlite.sh
@@ -9,7 +9,7 @@ echo "$URL"
 curl --output system.data.sqlite.zip "https://system.data.sqlite.org/blobs/$1/sqlite-netFx-source-$1.zip"
 unzip system.data.sqlite.zip
 
-dos2unix System.Data.SQLite/SQLite3.cs
+dos2unix System.Data.SQLite/System.Data.SQLite.2015.csproj
 dos2unix System.Data.SQLite/UnsafeNativeMethods.cs
 cd ..
 patch -p0 < patches/System.Data.SQLite/0001_osx-arm64_support.patch
